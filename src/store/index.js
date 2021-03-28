4c52062c1fd13983905178394abf4b04f556772c
Nako4 <papnoatelier@gmail.com>
2021-03-28 15:24:08 +0900

    カウントボタンと処理の記述

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 5
   },
   mutations: {
+    increment(state){
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    }
   },
   actions: {
+    // incrementが5秒後(5000)に実行されるような記述
+    increment: ({ commit }) => {
+      setTimeout(() => {
+        commit('increment')
+      }, 5000)
+    }
   },
   modules: {
   }
---
+++ src/views/Count.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="count">
+    <h1>This is an count page</h1>
+    <!-- Vuexのstoreにstateとして管理されているcountデータを取得 -->
+    {{ $store.state.count }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+      // store/index.jsに記述したmutationsを呼び出し(commit)
+      // dispatchで呼び出すと、store/index.jsのactionsが呼び出される(dispatch)→クリックして5秒後にカウントが反映される
+    increment() {
+      this.$store.dispatch("increment");
+    },
+    decrement() {
+      this.$store.commit("decrement");
+    }
+  }
+};
+</script>
